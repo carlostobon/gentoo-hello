6268a5ed00d48b2e21f501159cd26fa7091bc0b7
carlostobon <carlostobon@tutanota.de>
2025-03-14 14:44:33 -0500

    other commit

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
+  int sum;
   printf("Hello, Gentoo!\n");
   printf("Hello, Gentoo Again!\n");
+  sum = add(18, 29);
+  printf("Sum: %d\n", sum);
   return 0;
 }
